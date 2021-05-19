c3850c352e0605f4dc97175780f73ce7825523d8
Zhang Zisu <thezzisu@gmail.com>
2021-05-19 14:36:20 +0000

    feat: add dev2dev

--- components/app/AppNav.vue
+++ components/app/AppNav.vue
@@ -29,6 +29,8 @@ import {
 } from '@mdi/js'
 import AdminNav from '~/components/admin/AdminNav.vue'
 
+const dev2dev = "M11.84,19V16.41l2.34-.36v-.28H1V13.59h15.9l2.69-1.5v-.21L17,10.38H1V8.2H14.23V8l-2.39-.38V5L23,11.27V12.7Z"
+
 export default Vue.extend({
   name: 'AppNav',
   components: { AdminNav },
@@ -41,6 +43,11 @@ export default Vue.extend({
         { link: { to: '/changelog' }, icon: mdiUpdate, text: 'Changelog' },
         { link: { to: '/build' }, icon: mdiRocketLaunch, text: 'Build Info' },
         { link: { to: '/link' }, icon: mdiLink, text: 'Links' },
+        {
+          link: { href: 'https://dev2dev.pages.dev', target: '_blank' },
+          icon: dev2dev,
+          text: 'Dev2Dev'
+        },
         {
           link: { href: 'https://travellings.pages.dev', target: '_blank' },
           icon: mdiTrainVariant,
